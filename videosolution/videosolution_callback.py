119a310f5becca274dae070acf706d80cb00afa0
maxiaofeng <maxiaofeng@corp.netease.com>
2019-06-12 10:06:57 +0800

    注释非必要参数 && 新接口demo

--- videosolution/videosolution_callback.py
+++ videosolution/videosolution_callback.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""易盾视频检测接口python示例代码
+"""易盾点播音视频解决方案结果获取接口python示例代码
 接口文档: http://dun.163.com/api.html
 python版本：python2.7
 运行:
-    1. 修改 SECRET_ID,SECRET_KEY,BUSINESS_ID 为对应申请到的值
-    2. $ python check.py
+    1. 修改 SECRET_ID,SECRET_KEY 为对应申请到的值
+    2. $ python callback.py
 """
 __author__ = 'yidun-dev'
 __date__ = '2016/3/10'
@@ -18,21 +18,19 @@ import urllib
 import urllib2
 import json
 
-class VideoAPIDemo(object):
-    """视频检测接口示例代码"""
-    API_URL = "https://as.dun.163yun.com/v3/video/submit"
-    VERSION = "v3"
+class VideoSolutionCallbackAPIDemo(object):
+    """点播音视频解决方案检测结果获取接口示例代码"""
+    API_URL = "https://as.dun.163yun.com/v1/videosolution/callback/results"
+    VERSION = "v1"
 
-    def __init__(self, secret_id, secret_key, business_id):
+    def __init__(self, secret_id, secret_key):
         """
         Args:
             secret_id (str) 产品密钥ID，产品标识
             secret_key (str) 产品私有密钥，服务端生成签名信息使用
-            business_id (str) 业务ID，易盾根据产品业务特点分配
         """
         self.secret_id = secret_id
         self.secret_key = secret_key
-        self.business_id = business_id
 
     def gen_signature(self, params=None):
         """生成签名信息
@@ -47,15 +45,13 @@ class VideoAPIDemo(object):
         buff += self.secret_key
         return hashlib.md5(buff).hexdigest()
 
-    def check(self, params):
+    def check(self):
         """请求易盾接口
-        Args:
-            params (object) 请求参数
         Returns:
             请求结果，json格式
         """
+        params = {}
         params["secretId"] = self.secret_id
-        params["businessId"] = self.business_id
         params["version"] = self.VERSION
         params["timestamp"] = int(time.time() * 1000)
         params["nonce"] = int(random.random()*100000000)
@@ -64,7 +60,7 @@ class VideoAPIDemo(object):
         try:
             params = urllib.urlencode(params)
             request = urllib2.Request(self.API_URL, params)
-            content = urllib2.urlopen(request, timeout=1).read()
+            content = urllib2.urlopen(request, timeout=10).read()
             return json.loads(content)
         except Exception, ex:
             print "调用API接口失败:", str(ex)
@@ -73,20 +69,11 @@ if __name__ == "__main__":
     """示例代码入口"""
     SECRET_ID = "your_secret_id" # 产品密钥ID，产品标识
     SECRET_KEY = "your_secret_key" # 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露
-    BUSINESS_ID = "your_business_id" # 业务ID，易盾根据产品业务特点分配
-    api = VideoAPIDemo(SECRET_ID, SECRET_KEY, BUSINESS_ID)
+    api = VideoSolutionCallbackAPIDemo(SECRET_ID, SECRET_KEY)
+    
+    ret = api.check()
 
-    params = {
-        "dataId": "fbfcad1c-dba1-490c-b4de-e784c2691765",
-        "url": "http://xxx.xxx.com/xxxx"
-        # "callback": "{\"p\":\"xx\"}"
-    }
-
-    ret = api.check(params)
     if ret["code"] == 200:
-        if ret["result"]["status"]==0:
-            print  "推送成功!taskId=%s" % (ret["result"]["taskId"])
-        else:
-            print  "推送失败!taskId=%s" % (ret["result"]["taskId"])    
+        resultArray = ret["result"]
     else:
         print "ERROR: ret.code=%s, ret.msg=%s" % (ret["code"], ret["msg"])
