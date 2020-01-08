f60d547d87e449ee6d4582d18249f1e10b9041c8
maxiaofeng <maxiaofeng@corp.netease.com>
2020-01-08 17:18:44 +0800

    新增直播音频片段查询接口

--- audio/liveaudio_querytask.py
+++ audio/liveaudio_querytask.py
@@ -21,10 +21,10 @@ import json
 
 
 class LiveAudioCallbackAPIDemo(object):
-    """直播音频检测结果获取接口示例代码"""
+    """调用易盾反垃圾云服务查询直播语音片段离线结果接口API示例"""
 
-    API_URL = "https://as-liveaudio.dun.163yun.com/v1/liveaudio/callback/results"
-    VERSION = "v1.1"
+    API_URL = "https://as-liveaudio.dun.163yun.com/v1/liveaudio/query/task"
+    VERSION = "v1.0"
 
     def __init__(self, secret_id, secret_key, business_id):
         """
@@ -50,12 +50,11 @@ class LiveAudioCallbackAPIDemo(object):
         buff += self.secret_key
         return hashlib.md5(buff.encode("utf8")).hexdigest()
 
-    def check(self):
+    def check(self, params):
         """请求易盾接口
         Returns:
             请求结果，json格式
         """
-        params = {}
         params["secretId"] = self.secret_id
         params["businessId"] = self.business_id
         params["version"] = self.VERSION
@@ -78,8 +77,13 @@ if __name__ == "__main__":
     SECRET_KEY = "your_secret_key"  # 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露
     BUSINESS_ID = "your_business_id"  # 业务ID，易盾根据产品业务特点分配
     api = LiveAudioCallbackAPIDemo(SECRET_ID, SECRET_KEY, BUSINESS_ID)
-    
-    ret = api.check()
+
+    params = {
+        "taskId": xxx,
+        "startTime": 1578252600000,
+        "endTime": 1578253200000
+    }
+    ret = api.check(params)
 
     code: int = ret["code"]
     msg: str = ret["msg"]
@@ -91,20 +95,16 @@ if __name__ == "__main__":
             for result in resultArray:
                 taskId: str = result["taskId"]
                 asrStatus: int = result["asrStatus"]
-                if asrStatus == 4:
-                    asrResult: int = result["asrResult"]
-                    print("检测失败: taskId=%s, asrResult=%s" % (taskId, asrResult))
-                else:
-                    action: int = result["action"]
-                    segmentArray: list = result["segments"]
-                    startTime: int = result["startTime"]
-                    endTime: int = result["endTime"]
-                    if action == 0:
-                        print("taskId=%s, 结果: 通过, 证据信息如下: %s, startTime:%s, endTime:%s" % (taskId, segmentArray, startTime, endTime))
-                    elif action == 1 or action == 2:
-                        # for segmentItem in segmentArray:
-                        #     label: int = segmentItem["label"]
-                        #     level: int = segmentItem["level"]
-                        print("taskId=%s, 结果: %s，证据信息如下: %s, startTime:%s, endTime:%s" % (taskId, "不确定" if action == 1 else "不通过", segmentArray, startTime, endTime))
+                action: int = result["action"]
+                segmentArray: list = result["segments"]
+                startTime: int = result["startTime"]
+                endTime: int = result["endTime"]
+                if action == 0:
+                    print("taskId=%s, 结果: 通过, 证据信息如下: %s, startTime:%s, endTime:%s" % (taskId, segmentArray, startTime, endTime))
+                elif action == 1 or action == 2:
+                    # for segmentItem in segmentArray:
+                    #     label: int = segmentItem["label"]
+                    #     level: int = segmentItem["level"]
+                    print("taskId=%s, 结果: %s，证据信息如下: %s, startTime:%s, endTime:%s" % (taskId, "不确定" if action == 1 else "不通过", segmentArray, startTime, endTime))
     else:
         print("ERROR: code=%s, msg=%s" % (ret["code"], ret["msg"]))
