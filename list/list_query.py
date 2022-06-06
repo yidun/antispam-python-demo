6417196aad4ad85bb7355e0e47a545d9fb6f2ffc
sunlifang <sunlifang@corp.netease.com>
2022-06-06 18:56:58 +0800

    名单接口升级至v2版本，并增加查询、删除、更新接口

--- list/list_query.py
+++ list/list_query.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-易盾反垃圾云服务名单提交接口python示例代码
+易盾反垃圾云服务名单查询接口python示例代码
 接口文档: http://dun.163.com/api.html
 python版本：python3.7
 运行:
     1. 修改 SECRET_ID,SECRET_KEY,BUSINESS_ID 为对应申请到的值
-    2. $ python list_submit.py
+    2. $ python list_query.py
 """
 __author__ = 'yidun-dev'
-__date__ = '2020/01/02'
+__date__ = '2022/06/06'
 __version__ = '0.2-dev'
 
 import hashlib
@@ -21,20 +21,22 @@ import json
 from gmssl import sm3, func
 
 
-class KeywordSubmitAPIDemo(object):
-    """名单提交接口示例代码"""
+class ListQueryDemo(object):
+    """易盾反垃圾云服务名单查询接口python示例代码"""
 
-    API_URL = "http://as.dun.163.com/v2/list/submit"
+    API_URL = "http://as.dun.163yun.com/v2/list/pageQuery"
     VERSION = "v2"
 
-    def __init__(self, secret_id, secret_key):
+    def __init__(self, secret_id, secret_key, business_id):
         """
         Args:
             secret_id (str) 产品密钥ID，产品标识
             secret_key (str) 产品私有密钥，服务端生成签名信息使用
+            business_id (str) 业务ID，易盾根据产品业务特点分配
         """
         self.secret_id = secret_id
         self.secret_key = secret_key
+        self.business_id = business_id
 
     def gen_signature(self, params=None):
         """生成签名信息
@@ -52,7 +54,7 @@ class KeywordSubmitAPIDemo(object):
         else:
             return hashlib.md5(buff.encode("utf8")).hexdigest()
 
-    def check(self, params):
+    def query(self, params):
         """请求易盾接口
         Args:
             params (object) 请求参数
@@ -60,6 +62,7 @@ class KeywordSubmitAPIDemo(object):
             请求结果，json格式
         """
         params["secretId"] = self.secret_id
+        params["businessId"] = self.business_id
         params["version"] = self.VERSION
         params["timestamp"] = int(time.time() * 1000)
         params["nonce"] = int(random.random() * 100000000)
@@ -69,7 +72,7 @@ class KeywordSubmitAPIDemo(object):
         try:
             params = urlparse.urlencode(params).encode("utf8")
             request = urlrequest.Request(self.API_URL, params)
-            content = urlrequest.urlopen(request, timeout=1).read()
+            content = urlrequest.urlopen(request, timeout=10).read()
             return json.loads(content)
         except Exception as ex:
             print("调用API接口失败:", str(ex))
@@ -79,27 +82,37 @@ if __name__ == "__main__":
     """示例代码入口"""
     SECRET_ID = "your_secret_id"  # 产品密钥ID，产品标识
     SECRET_KEY = "your_secret_key"  # 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露
-    api = KeywordSubmitAPIDemo(SECRET_ID, SECRET_KEY)
+    BUSINESS_ID = "your_business_id"  # 业务ID，易盾根据产品业务特点分配
+    api = ListQueryDemo(SECRET_ID, SECRET_KEY, BUSINESS_ID)
 
     # 私有请求参数
-    lists: list = ["用户名单1", "用户名单2"]
     params = {
-        "listType": "2",  # 1: 白名单，2: 黑名单，4: 必审名单，8: 预审名单
-        "entityType": "1",  # 1: 用户名单，2: IP名单
-        "entities": json.dumps(lists)
+        "pageNum": 1,
+        "pageSize": 20,
+        "startTime": 1598951727666,
+        "endTime": 1598961727666,
+        "listType": 2,
+        "entityType": 1
     }
 
-    ret = api.check(params)
+    ret = api.query(params)
 
     code: int = ret["code"]
     msg: str = ret["msg"]
     if code == 200:
-        resultArray: list = ret["result"]
-        for result in resultArray:
-            uuid: str = result["uuid"]
-            entity: str = result["entity"]
-            entityType: int = result["entityType"]
-            exist: bool = ret["exist"]
-            print("名单是否已存在: %s" % exist)
+        result: dict = ret["result"]
+        count: int = result["count"]
+        rows: list = result["rows"]
+        if rows is not None and len(rows) > 0:
+            for row in rows:
+                listType: int = row["listType"]
+                entityType: int = row["entityType"]
+                productId: int = row["productId"]
+                targetId: int = row["targetId"]
+                entity: str = row["entity"]
+                releaseTime: int = row["releaseTime"]
+                source: int = row["source"]
+                spamType: int = row["spamType"]
+        print("count:%s, rows:%s" % (count, rows))
     else:
         print("ERROR: code=%s, msg=%s" % (ret["code"], ret["msg"]))
