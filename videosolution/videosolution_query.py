5c139f48dcbc41102387e58a7d0dfde50ace5d20
liangtingchen <liangtingchen@corp.netease.com>
2022-10-26 17:35:31 +0800

    更新点播音视频 v2 查询接口

--- videosolution/videosolution_query.py
+++ videosolution/videosolution_query.py
@@ -24,8 +24,8 @@ from gmssl import sm3, func
 class VideoSolutionQueryAPIDemo(object):
     """点播音视频解决方案结果查询接口示例代码"""
 
-    API_URL = "http://as.dun.163.com/v1/videosolution/query/task"
-    VERSION = "v1"
+    API_URL = "http://as.dun.163.com/v2/videosolution/query/task"
+    VERSION = "v2"
 
     def __init__(self, secret_id, secret_key):
         """
@@ -79,9 +79,11 @@ if __name__ == "__main__":
     SECRET_KEY = "your_secret_key"  # 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露
     api = VideoSolutionQueryAPIDemo(SECRET_ID, SECRET_KEY)
 
-    taskIds: list = ['202b1d65f5854cecadcb24382b681c1a', '0f0345933b05489c9b60635b0c8cc721']  # 查询参数taskIds
+    taskIds: list = ['hvzrbqcl2zrr53inegdsh5tg02009x8w']  # 查询参数taskIds
+    dataIds: list = ['dataId85480']  # 查询参数dataIds
     params = {
-        "taskIds": taskIds
+        "taskIds": taskIds,
+        "dataIds": dataIds
     }
 
     ret = api.check(params)
@@ -94,43 +96,71 @@ if __name__ == "__main__":
             print("暂时没有结果需要获取, 请稍后重试!")
         else:
             for resultItem in resultArray:
-                taskId: str = resultItem["taskId"]
+                taskId: str = resultItem["taskId"] if "taskId" in resultItem else ''
                 status: int = resultItem["status"]
+                dataId = resultItem["dataId"] if "dataId" in resultItem else ''
+                print("点播音视频 taskId:%s, dataId:%s, 数据状态%s" % (taskId, dataId, status))
                 if status == 0:
-                    result: int = resultItem["result"]
-                    print("点播音视频, taskId:%s, 检测结果:%s" % (taskId, result))
-                    if resultItem["evidences"] is not None:
-                        evidences: dict = resultItem["evidences"]
-                        if evidences["texts"] is not None:
-                            texts: list = evidences["texts"]
-                            for textItem in texts:
-                                print("文本信息, dataId:%s, 检测结果:%s", textItem["dataId"], textItem["action"])
-                        elif evidences["images"] is not None:
-                            images: list = evidences["images"]
-                            for imageItem in images:
-                                print("图片信息, dataId:%s, 检测状态:%s, 检测结果:%s", imageItem["dataId"], imageItem["status"],
-                                      imageItem["action"])
-                        elif evidences["audios"] is not None:
-                            audios: list = evidences["audios"]
-                            for audioItem in audios:
-                                print("语音信息, dataId:%s, 检测状态:%s, 检测结果:%s", audioItem["dataId"], audioItem["asrStatus"],
-                                      audioItem["action"])
-                        elif evidences["videos"] is not None:
-                            videos: list = evidences["videos"]
-                            for videoItem in videos:
-                                print("视频信息, dataId:%s, 检测状态:%s, 检测结果:%s", videoItem["dataId"], videoItem["status"],
-                                      videoItem["level"])
-                    elif resultItem["reviewEvidences"] is not None:
-                        reviewEvidences: dict = resultItem["reviewEvidences"]
-                        reason: str = reviewEvidences["reason"]
-                        detail: dict = reviewEvidences["detail"]
-                        text: list = reviewEvidences["text"]
-                        image: list = reviewEvidences["image"]
-                        audio: list = reviewEvidences["audio"]
-                        video: list = reviewEvidences["video"]
-                elif status == 20:
-                    print("点播音视频, taskId:%s, 数据非7天内", taskId)
-                elif status == 30:
-                    print("点播音视频, taskId:%s, 数据不存在", taskId)
+                    if "antispam" in resultItem:
+                        antispam: dict = resultItem["antispam"]
+                        antispamTaskId: str = antispam["taskId"]
+                        callback: str = antispam["callback"] if "callback" in antispam else ''
+                        suggestion: int = antispam["suggestion"]
+                        status: int = antispam["status"]
+                        resultType: int = antispam["resultType"]
+                        censorRound: int = antispam["censorRound"] if "censorRound" in antispam else 1
+                        checkTime: int = antispam["checkTime"]
+                        censorTime: int = antispam["censorTime"]
+                        duration: int = antispam["duration"]
+                        print("点播音视频反垃圾结果, taskId:%s, callback:%s, 检测状态:%s, 检测结果:%s" % (
+                            antispamTaskId, callback, status, suggestion))
+                        if "evidences" in antispam:
+                            evidences: dict = antispam["evidences"]
+                            if "text" in evidences:
+                                textItem: dict = evidences["text"]
+                                print("文本信息, dataId:%s, 检测结果:%s" % (textItem["dataId"], textItem["suggestion"]))
+                            if "images" in evidences:
+                                images: list = evidences["images"]
+                                for imageItem in images:
+                                    print("图片信息, taskId:%s, 检测状态:%s, 检测结果:%s" % (imageItem["taskId"],
+                                          imageItem["status"], imageItem["suggestion"]))
+                            if "audio" in evidences:
+                                audioItem: dict = evidences["audio"]
+                                print("语音信息, taskId:%s, 检测状态:%s, 检测结果:%s" % (audioItem["taskId"],
+                                      audioItem["status"], audioItem["suggestion"]))
+                            if "video" in evidences:
+                                videoItem: dict = evidences["video"]
+                                print("视频信息, taskId:%s, 检测状态:%s, 检测结果:%s" % (videoItem["taskId"],
+                                      videoItem["status"], videoItem["suggestion"]))
+                        elif "reviewEvidences" in antispam:
+                            reviewEvidences: dict = antispam["reviewEvidences"]
+                            description: str = reviewEvidences[
+                                "description"] if "description" in reviewEvidences else ''
+                            detail: str = reviewEvidences["detail"] if "detail" in reviewEvidences else ''
+                            spamType: int = reviewEvidences["spamType"] if "spamType" in reviewEvidences else -1
+                            texts: list = reviewEvidences["texts"] if "texts" in reviewEvidences else []
+                            images: list = reviewEvidences["images"] if "images" in reviewEvidences else []
+                            audios: list = reviewEvidences["audios"] if "audios" in reviewEvidences else []
+                            videos: list = reviewEvidences["videos"] if "videos" in reviewEvidences else []
+                            print("人审证据信息，备注%s, 垃圾类型%s, 文本证据%s, 图片证据%s, 音频证据%s, 视频证据%s" %
+                                  (description, spamType, texts, images, audios, videos))
+                    if "language" in resultItem:
+                        language: dict = resultItem["language"]
+                        languageDetails: list = language["details"] if "details" in language else []
+                        print("语种检测结果详情:", languageDetails)
+                    if "voice" in resultItem:
+                        voice: dict = resultItem["voice"]
+                        voiceDetails: list = voice["details"] if "details" in voice else []
+                        print("人声属性检测结果详情:", voiceDetails)
+                    if "asr" in resultItem:
+                        asr: dict = resultItem["asr"]
+                        asrDetails: list = asr["details"] if "details" in asr else []
+                        print("语音识别检测结果详情:", asrDetails)
+                elif status == 1:
+                    print("点播音视频, taskId:%s, 数据非7天内" % taskId)
+                elif status == 2:
+                    print("点播音视频, taskId:%s, 数据不存在" % taskId)
+                elif status == 3:
+                    print("点播音视频, taskId:%s, 检测中" % taskId)
     else:
         print("ERROR: code=%s, msg=%s" % (ret["code"], ret["msg"]))
